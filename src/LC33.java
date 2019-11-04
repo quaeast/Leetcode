1c7ab98cde6ef7c80432b1f8c0125649ae52f118
quaeast <424044876@qq.com>
2019-11-04 14:36:33 +0800

    33

---
+++ src/LC33.java
@@ -0,0 +1,13 @@
+class Solution33 {
+    public int search(int[] nums, int target) {
+        for (int i=0; i<nums.length; i++){
+            if(nums[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+public class LC33 {
+}
