d1cf40622f5c0a6ce2d785f9b14b96f017dea77b
quaeast <424044876@qq.com>
2019-11-08 10:54:13 +0800

    32

---
+++ src/LC32.java
@@ -0,0 +1,29 @@
+import com.sun.tools.classfile.Opcode;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
+class Solution32 {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack();
+        stack.add(-1);
+        int max=0;
+        for (int i=0;i<s.length();i++){
+            char t = s.charAt(i);
+            if(t=='('){
+                stack.push(i);
+            }else {
+                stack.pop();
+                if(!stack.isEmpty()){
+                    max = Math.max(max, i-stack.peek());
+                }else {
+                    stack.push(i);
+                }
+            }
+        }
+        return max;
+    }
+}
+
+public class LC32 {
+}
--- src/Test.java
+++ src/Test.java
@@ -1,34 +1,7 @@
-class Pair extends Object{
-    int a;
-    int b;
+public class Test{
 
-    Pair(){}
-    Pair(int a, int b){
-        this.a = a;
-        this.b = b;
-    }
 
-    @Override
-    public boolean equals(Object obj){
-        Pair that = (Pair) obj;
-        if(this.a==that.a&&this.b==that.b){
-            return true;
-        }else {
-            return false;
-        }
-    }
-}
-public class Test {
-    public static void swap(Integer a, Integer b){
-        int t = a;
-        a = b;
-        b = a;
-    }
     public static void main(String[] args){
-        int a=10;
-        int b=11;
-        swap(a, b);
-        System.out.println(a);
-        System.out.println(b);
+
     }
 }
