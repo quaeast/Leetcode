bb923edac576d5b28d7071d5789880dd494829fc
quaeast <424044876@qq.com>
2019-11-09 10:19:35 +0800

    38

--- src/LC38.java
+++ src/LC38.java
@@ -1,21 +1,35 @@
-//class Solution {
-//    public String countAndSay(String n) {
-//        StringBuffer res = new StringBuffer();
-//        int flag = 0;
-//        for (int i = 1; i < n.length(); i++) {
-//            if(n.charAt(i)!=n.charAt(i-1)){
-//
-//            }
-//        }
-//    }
-//
-//    public String countAndSay(int n) {
-//        if (n == 1) {
-//            return "1";
-//        }
-//        return countAndSay(n - 1);
-//    }
-//}
-//
-//public class LC38 {
-//}
+class Solution38 {
+    public String countAndSay(String n) {
+        int flag = 1;
+        StringBuffer res = new StringBuffer();
+        for (int i = 1; i < n.length(); i++) {
+            if(n.charAt(i)==n.charAt(i-1)){
+                flag++;
+            }else{
+                res.append(Integer.toString(flag));
+                res.append(n.charAt(i-1));
+                flag=1;
+            }
+        }
+        if(flag!=0){
+            res.append(Integer.toString(flag));
+            res.append(n.charAt(n.length()-1));
+        }
+        return res.toString();
+    }
+
+    public String countAndSay(int n) {
+        String res = "1";
+        for (int i = 0; i < n - 1; i++) {
+            res = countAndSay(res);
+        }
+        return res;
+    }
+}
+
+public class LC38 {
+    public static void main(String[] args){
+        Solution38 s = new Solution38();
+        System.out.println(s.countAndSay(5));
+    }
+}
