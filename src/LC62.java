576c624f335a08aa558de16916d802c3c67d68b8
fangzidong <424044876@qq.com>
2020-11-26 18:44:49 +0800

    62 dp

--- src/LC62.java
+++ src/LC62.java
@@ -1,5 +1,4 @@
 class Solution62 {
-
     public int uniquePaths0(int m, int n, int currentX, int currentY, int routeSum) {
         if (currentX + 1 == m && currentY + 1 == n) {
             return ++routeSum;
@@ -14,6 +13,24 @@ class Solution62 {
 
     public int uniquePaths(int m, int n) {
         return uniquePaths0(m, n, 0, 0, 0);
+    }
+}
+
+class Solution62_2 {
+    public int uniquePaths(int m, int n) {
+        int[][] panel = new int[m][n];
+        for (int i = 0; i < m; i++) {
+            panel[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            panel[0][j] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                panel[i][j] = panel[i-1][j]+panel[i][j-1];
+            }
+        }
+        return panel[m-1][n-1];
     }
 }
 
